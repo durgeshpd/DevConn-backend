dab1225fe8299a817fb2c825abc7301145a87dcb
Durgesh Pandey <41140945+durgeshpd@users.noreply.github.com>
2025-04-29 14:51:16 +0530

    update 2

---
+++ src/routes/user.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const userRouter = express.Router();
+
+const { userAuth } = require('../middlewares/auth');
+const ConnectionRequest = require("../models/connectionRequest")
+const User = require("../models/user");
+
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            toUserId: loggedInUser._id,
+            status: "accepted",
+        }).populate("fromUserId", ["firstName", "lastName"]);
+
+        res.json({
+            message: "Data fetched successfully",
+            data: connectionRequests,
+        });
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message);
+    }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { toUserId: loggedInUser._id, status: "accepted" },
+                { fromUserId: loggedInUser._id, status: "accepted" },
+            ],
+        });
+        res.json({ data: connectionRequests });
+    } catch (err) {
+        res.status(400).send({ message: err.message });
+    }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+    try {
+
+        const loggedInUser = req.user;
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+        }).select("fromUserId toUserId");
+
+        const hideUsersFromFeed = new Set();
+        connectionRequests.forEach((req) => {
+            hideUsersFromFeed.add(req.fromUserId.toString());
+            hideUsersFromFeed.add(req.toUserId.toString());
+        })
+        const users = await User.find({
+            $and: [
+               { _id: { $nin: Array.from(hideUsersFromFeed) }},
+               { _id: { $ne: loggedInUser._id}},
+            ],
+        });
+
+        res.send(users);
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
+})
+
+module.exports = userRouter;
